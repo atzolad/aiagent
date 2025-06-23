1362c9be18ef2a03cad6500de03bf4dfa3e1af52
atzolad <atzolad@gmail.com>
2025-06-23 16:18:24 -0700

    Added Calculator app for agent to work with as well as a prompt flag for additional info

--- main.py
+++ main.py
@@ -1,31 +1,47 @@
 import os
 from dotenv import load_dotenv
 import sys
+from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
-from google import genai
-
 client = genai.Client(api_key=api_key)
 
-if len(sys.argv) <1:
+if len(sys.argv) <= 1:
     print('Invalid Prompt')
     sys.exit(1)
 
 else:
-    prompt = sys.argv[1]
+    user_prompt = sys.argv[1] #Take a command line input for the prompt.
 
+
+#Keep a list to record the messages that have already been sent to the AI agent
+messages = [
+    types.Content(role="user", parts =[types.Part(text=user_prompt)]),
+]
+
+#Send the prompt to the AI agent
 response = client.models.generate_content(
     model = "gemini-2.0-flash-001",
-    contents = prompt
+    contents = messages,
 )
+
+
 print(response.text)
+
+#Keep track of the API usage data
 if response.usage_metadata:
 
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
-    print(f"Prompt tokens: {prompt_tokens}")
-    print(f"Response tokens: {response_tokens}")
+
 else:
     print("Usage Metadata not available in the response")
+
+if sys.argv[-1] == "--verbose":
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(f"Response tokens: {response_tokens}")
+
