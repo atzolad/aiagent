37eae86867f0f90b19e01d0ddf13675ba184f1fd
atzolad <atzolad@gmail.com>
2025-06-23 19:04:01 -0700

    Fixed get_files_info function and added tests file

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,28 +1,41 @@
 import os
 
 def get_files_info(working_directory, directory=None):
-    abs_directory = os.path.abspath(directory)
+    relative_directory = os.path.join(working_directory, directory)
+    abs_directory = os.path.abspath(relative_directory)
     abs_working_directory = os.path.abspath(working_directory)
 
-    if not abs_directory.startswith(abs_working_directory):
-        return (f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
 
-    if not os.path.isdir(directory):
-        return (f'Error: "{directory}" is not a directory')
-    
-    else:
+    try:
+
+        if not abs_directory.startswith(abs_working_directory):
+            return (f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+            #print(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+
+        if not os.path.isdir(abs_directory):
+            return (f'Error: "{directory}" is not a directory')
+            #print(f'Error: "{directory}" is not a directory')
         
-        contents = os.listdir(directory)
-        contents_list = []
+        else:
+            
+            contents = os.listdir(abs_directory)
+            contents_list = []
 
-        for file in contents:
-            file_name = file
-            file_path = os.path.join(directory, file)
-            file_size = os.path.getsize(file_path)
-            is_file_dir = os.path.isdir(file_path)
-            #print(f"- {file_name}: file_size={file_size} bytes, is_dir={is_file_dir}")
-            contents_list.append(f"- {file_name}: file_size={file_size} bytes, is_dir={is_file_dir}")
+            for file in contents:
+                file_name = file
+                file_path = os.path.join(abs_directory, file)
+                file_size = os.path.getsize(file_path)
+                is_file_dir = os.path.isdir(file_path)
+                #print(f"- {file_name}: file_size={file_size} bytes, is_dir={is_file_dir}")
+                contents_list.append(f"- {file_name}: file_size={file_size} bytes, is_dir={is_file_dir}")
 
-        print('\n'.join(contents_list))
+            return('\n'.join(contents_list))
+            #print(('\n'.join(contents_list)))
 
-get_files_info("/Users/alexzolad/workspace/github.com/atzolad/aiagent", "/Users/alexzolad/workspace/github.com/atzolad/aiagent")
+    except Exception as e:
+        return(f"Error: {e}")
+
+'''get_files_info("calculator", ".")
+get_files_info("calculator", "pkg")
+get_files_info("calculator", "/bin")
+get_files_info("calculator", "../")'''
